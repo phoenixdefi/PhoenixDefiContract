aa1d5453abf1b90666b2ed3426a59af202ee73a6
Roger <phoenixdefi.finance@gmail.com>
2021-06-17 20:10:43 +0700

    Update MasterChef & PNIXS contracts
    Update MasterChef & PNIXS contracts to fix bugs found by Certik

--- PhoenixToken.sol
+++ PhoenixToken.sol
@@ -586,7 +586,7 @@ contract PhoenixToken is Context, IBEP20, Ownable {
     }
 
     function includeAccount(address account) external onlyOwner() {
-        require(_isExcluded[account], "Account is already excluded");
+        require(_isExcluded[account], "Account is not excluded");
         for (uint256 i = 0; i < _excluded.length; i++) {
             if (_excluded[i] == account) {
                 _excluded[i] = _excluded[_excluded.length - 1];
